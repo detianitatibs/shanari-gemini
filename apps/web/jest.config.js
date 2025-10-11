2414cb63b9ac4680ed57db1bae298ea7c1e61b72
Keisuke Tokuda <tokuda.keisuke.pm@gmail.com>
2025-10-11 11:05:55 +0900

    ビルド時のwarningを解決するように依存関係を見直し

--- apps/web/jest.config.js
+++ apps/web/jest.config.js
@@ -9,6 +9,10 @@ const createJestConfig = nextJest({
 const customJestConfig = {
   setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
   testEnvironment: 'jest-environment-jsdom',
+  reporters: [
+    'default',
+    ['jest-junit', { outputDirectory: 'reports', outputName: 'jest-junit.xml' }],
+  ],
 }
 
 // createJestConfig is exported this way to ensure that next/jest can load the Next.js config which is async
